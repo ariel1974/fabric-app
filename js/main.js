f34a910585dd621ea318cafb926ee9939e85aa0a
arielillouz <ariel.illouz@gmail.com>
2015-05-27 23:16:42 +0300

    init commit

--- js/main.js
+++ js/main.js
@@ -1,25 +1,106 @@
-var app = {
+function initPushwoosh() {
+	var pushNotification = cordova.require("com.pushwoosh.plugins.pushwoosh.PushNotification");
+	if(device.platform == "Android")
+	{
+		registerPushwooshAndroid();
+	}
 
-    findByName: function() {
-        var self = this;
-        this.store.findByName($('.search-key').val(), function(employees) {
-            $('.employee-list').html(self.employeeLiTpl(employees));
-        });
-    },
+	if(device.platform == "iPhone" || device.platform == "iOS")
+	{
+		registerPushwooshIOS();
+	}
+
+	if (device.platform == "Win32NT") {
+	    registerPushwooshWP();
+	}
+}
+
+var app = {
 
     initialize: function() {
 
-
-        this.homeTpl = Handlebars.compile($("#home-tpl").html());
-        this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
-
         var self = this;
         this.store = new MemoryStore(function() {
+            $('body').html(new HomeView(self.store).render().el);
 
-            //self.showAlert('Store Initialized', 'Info');
-            self.renderHomeView();
+            self.registerEvents();
         });
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
+    },
+
+    registerEvents: function() {
+        var self = this;
+        // Check of browser supports touch events...
+        if (document.documentElement.hasOwnProperty('ontouchstart')) {
+            // ... if yes: register touch event listener to change the "selected" state of the item
+            $('body').on('touchstart', 'a', function(event) {
+                $(event.target).addClass('tappable-active');
+            });
+            $('body').on('touchend', 'a', function(event) {
+                $(event.target).removeClass('tappable-active');
+            });
+        } else {
+            // ... if not: register mouse events instead
+            $('body').on('mousedown', 'a', function(event) {
+                $(event.target).addClass('tappable-active');
+            });
+            $('body').on('mouseup', 'a', function(event) {
+                $(event.target).removeClass('tappable-active');
+            });
+        }
+
+        // Bind any events that are required on startup. Common events are:
+        // 'load', 'deviceready', 'offline', and 'online'.
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicity call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        initPushwoosh();
+        app.receivedEvent('deviceready');
+    },
+
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    },
+
+    // Android
+    onNotificationGCM: function(e) {
+        switch( e.event )
+        {
+            case 'registered':
+                if ( e.regid.length > 0 )
+                {
+                    // Your GCM push server needs to know the regID before it can push to this device
+                    // here is where you might want to send it the regID for later use.
+                    alert('registration id = '+e.regid);
+                }
+            break;
+
+            case 'message':
+              // this is the actual push notification. its format depends on the data model
+              // of the intermediary push server which must also be reflected in GCMIntentService.java
+              alert('message = '+e.message+' msgcnt = '+e.msgcnt);
+            break;
+
+            case 'error':
+              alert('GCM error = '+e.msg);
+            break;
+
+            default:
+              alert('An unknown GCM event has occurred');
+              break;
+        }
     },
 
     showAlert: function (message, title) {
