97fac435f5359dd4f51ac5611ea9da39b7ff3be8
arielillouz <ariel.illouz@gmail.com>
2015-05-28 10:35:06 +0300

    init commit

--- www/js/index.js
+++ www/js/index.js
@@ -43,12 +43,7 @@ var app = {
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
-        //document.addEventListener('deviceready', this.onDeviceReady, true);
-
-        document.addEventListener("deviceready", function(){
-            alert("deviceready");
-            initPushwoosh();
-         }, true);
+        document.addEventListener('deviceready', this.onDeviceReady, true);
     },
     // deviceready Event Handler
     //
